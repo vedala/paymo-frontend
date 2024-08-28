39e23e48a0cdbae953416598b8267783d818d204
Kalyan Vedala <svedala23@gmail.com>
2024-08-28 05:07:31 -0700

    Move BrowserRouter to index to wrap Auth0ProviderWithNavigate

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Welcome from './components/Welcome';
 import Dashboard from './components/Dashboard';
@@ -12,15 +8,13 @@ import CallbackPage from './components/CallbackPage';
 function App() {
 
   return (
-    <Router>
-      <div className="App">
-        <Routes>
+    <div className='App'>
+      <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/callback" element={<CallbackPage />} />
-        </Routes>
-      </div>
-    </Router>
+      </Routes>
+    </div>
   );
 }
 
