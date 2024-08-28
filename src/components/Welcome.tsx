a9ab9f0da15e942e39fe334abb4004aa6446da10
Kalyan Vedala <svedala23@gmail.com>
2024-08-28 03:32:24 -0700

    Create Welcome page

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+
+function Welcome() {
+  return (
+    <div className="welcome">
+      <Header />
+      <h1>Welcome to Paymo!</h1>
+    </div>
+  );
+}
+
+export default Welcome;
