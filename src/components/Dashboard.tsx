223b3586c2456a686aa044bbfb3e73b99fce84ed
Kalyan Vedala <svedala23@gmail.com>
2024-08-27 21:34:38 -0700

    Create Header component, include in Dashboard page

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from './Header';
 
 function Dashboard() {
   return (
     <div className="dashboard">
+      <Header />
       <h1>Paymo Dashboard</h1>
     </div>
   );
