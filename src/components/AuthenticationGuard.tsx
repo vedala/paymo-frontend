c3ea80e3e5fb7b73dc02c142834f103f2b988622
Kalyan Vedala <svedala23@gmail.com>
2024-08-29 06:09:07 -0700

    Add configuration object wih onRedirecting

--- src/components/AuthenticationGuard.tsx
+++ src/components/AuthenticationGuard.tsx
@@ -2,7 +2,13 @@ import { withAuthenticationRequired } from "@auth0/auth0-react";
 import React from "react";
 
 const AuthenticationGuard = ({ component } : any) => {
-  const Component = withAuthenticationRequired(component);
+  const Component = withAuthenticationRequired(component, {
+    onRedirecting: () => (
+      <div>
+        Loading...
+      </div>
+    ),
+  });
 
   return <Component />;
 };
