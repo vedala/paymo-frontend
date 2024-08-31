17fcd944252b79c2c36abf9e0b06511a8758faf1
Kalyan Vedala <svedala23@gmail.com>
2024-08-31 06:09:40 -0700

    Create PageNotFound and add to Routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Welcome from './components/Welcome';
 import Dashboard from './components/Dashboard';
 import CallbackPage from './components/CallbackPage';
 import AuthenticationGuard from './components/AuthenticationGuard';
+import PageNotFound from './components/PageNotFound';
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
           element={<AuthenticationGuard component={Dashboard} />}
         />
         <Route path="/callback" element={<CallbackPage />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </div>
   );
---
+++ src/components/PageNotFound.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <NavBar />
+      <h1>
+        Page Not Found
+      </h1>
+    </div>
+  );
+};
+
+export default PageNotFound;
