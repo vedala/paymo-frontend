5dd2b914af12a2dea7821c43311353c279c0e0a4
Kalyan Vedala <svedala23@gmail.com>
2024-08-31 06:13:00 -0700

    Create PageNotFound component, add to Routes

--- src/components/PageNotFound.tsx
+++ src/components/PageNotFound.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import NavBar from "./NavBar";
+import { useAuth0 } from '@auth0/auth0-react';
+import GoToAppButton from './GoToAppButton';
 
 const PageNotFound = () => {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <div>
       <NavBar />
-      <h1>
-        Page Not Found
-      </h1>
+      <h1>Page Not Found</h1>
+      { isAuthenticated && (
+        <>
+          <GoToAppButton />
+        </>
+      )}
     </div>
   );
 };
