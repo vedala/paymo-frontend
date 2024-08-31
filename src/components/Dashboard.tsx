ee63edac2ae7be168864d36e34842b753a1b7764
Kalyan Vedala <svedala23@gmail.com>
2024-08-31 07:43:09 -0700

    Add tabs with basic placeholders

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import NavBar from './NavBar';
+import Tabs from './Tabs';
+import TabPane from './TabPane';
 
 function Dashboard() {
   return (
     <div className="dashboard">
       <NavBar />
       <h1>Paymo Dashboard</h1>
+      <Tabs>
+        <TabPane title="Banks">
+          <div>Banks</div>
+        </TabPane>
+        <TabPane title="Recipients">
+          <div>Recipients</div>
+        </TabPane>
+        <TabPane title="Activity">
+          <div>Activity</div>
+        </TabPane>
+        <TabPane title="Send Money">
+          <div>Send Money</div>
+        </TabPane>
+      </Tabs>
     </div>
   );
 }
