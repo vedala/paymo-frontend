178f9c349fae647f6a6c407701b9bfbeb91cfdd3
Kalyan Vedala <svedala23@gmail.com>
2024-09-04 07:51:56 -0700

    Display hard-coded bank list in component

--- src/components/Banks.tsx
+++ src/components/Banks.tsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Banks = () => {
 
+  interface BanksObject {
+    id: number,
+    name: string
+  }
+
+  const [banks, setBanks] = useState<Array<BanksObject>>([]);
+
+  useEffect(() => {
+    setBanks([
+      {
+        id: 1,
+        name: "Bank1",
+      },
+      {
+        id: 2,
+        name: "Bank2",
+      }
+    ]);
+  }, []);
+
+  const bankList = banks.map((bank) => {
+    return (
+      <li key={bank.id}>
+        <span>{bank.name}</span>
+      </li>
+    );
+  });
+
   return (
     <div>
-      Banks Page
+      <h2>Connected Banks</h2>
+      <ul>{bankList}</ul>
     </div>
   );
 };
