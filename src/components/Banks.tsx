b11f580b13148552bd4b6d28202fb03886f3d2b0
Kalyan Vedala <svedala23@gmail.com>
2024-09-04 08:23:05 -0700

    Get banks list from backend - in progress

--- src/components/Banks.tsx
+++ src/components/Banks.tsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-react";
+import { PAYMO_API_URL } from "../constants";
 
 const Banks = () => {
+
+  const { getAccessTokenSilently } = useAuth0();
 
   interface BanksObject {
     id: number,
@@ -10,16 +15,28 @@ const Banks = () => {
   const [banks, setBanks] = useState<Array<BanksObject>>([]);
 
   useEffect(() => {
-    setBanks([
-      {
-        id: 1,
-        name: "Bank1",
-      },
-      {
-        id: 2,
-        name: "Bank2",
-      }
-    ]);
+    async function fetchData() {
+      const token = await getAccessTokenSilently();
+      await axios.get(`${PAYMO_API_URL}/banks`, {
+        headers: { 'Authorization': `Bearer ${token}`}
+      })
+      .then(res => {
+        const bankData = res.data;
+        setBanks(bankData);
+      })
+      .catch(err => {console.log(err); throw err; })
+    };
+    fetchData();
+    // setBanks([
+    //   {
+    //     id: 1,
+    //     name: "Bank1",
+    //   },
+    //   {
+    //     id: 2,
+    //     name: "Bank2",
+    //   }
+    // ]);
   }, []);
 
   const bankList = banks.map((bank) => {
