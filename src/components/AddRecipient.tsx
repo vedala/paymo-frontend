0fbc5795122cfd1291192480f83c15f6d3bc6d28
Kalyan Vedala <svedala23@gmail.com>
2024-09-21 05:12:14 -0700

    Add input and search button

--- src/components/AddRecipient.tsx
+++ src/components/AddRecipient.tsx
@@ -1,10 +1,14 @@
 const AddRecipient = ({ onCancelClick }: any) => {
 
   return (
-    <div>
+    <div className="add-recipient">
       <h2>Add Recipient</h2>
-      <button onClick={onCancelClick} >Cancel</button>
-      <button>Save</button>
+      <input type="text" name="recipient-email" placeholder="Recipient Email"/>
+      <button>Search</button>
+      <div className="button-bar">
+        <button onClick={onCancelClick} >Cancel</button>
+        <button>Save</button>
+      </div>
     </div>
   );
 };
