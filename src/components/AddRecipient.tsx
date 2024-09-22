1b1fe51b6613f8add28e5cd27f6d768212ba8de0
Kalyan Vedala <svedala23@gmail.com>
2024-09-22 07:08:26 -0700

    AddRecipient component add markup, call backend to search user

--- src/components/AddRecipient.tsx
+++ src/components/AddRecipient.tsx
@@ -1,10 +1,41 @@
+import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-react";
+import { PAYMO_API_URL } from "../constants";
+import { useState } from "react";
+
 const AddRecipient = ({ onCancelClick }: any) => {
+
+  const [foundUserData, setFoundUserData] = useState([]);
+
+  const { getAccessTokenSilently } = useAuth0();
+
+  const fetchUsers = async () => {
+    const token = await getAccessTokenSilently();
+    await axios.get(`${PAYMO_API_URL}/get_user_by_email`, {
+      headers: { 'Authorization': `Bearer ${token}`}
+    })
+    .then(res => {
+      const resUsers = res.data;
+      setFoundUserData(resUsers);
+console.log("resUsers=", resUsers);
+    })
+    .catch(err => {console.log(err); throw err; })
+  }
+
+  const handleFormSubmit = (e: any) => {
+    e.preventDefault();
+console.log(e.target[0].value);
+    fetchUsers();
+  }
 
   return (
     <div className="add-recipient">
       <h2>Add Recipient</h2>
-      <input type="text" name="recipient-email" placeholder="Recipient Email"/>
-      <button>Search</button>
+      <form onSubmit={handleFormSubmit}>
+        <input type="email" name="recipient-email" required placeholder="Recipient Email"/>
+        <button>Search</button>
+        {"UserData:" + foundUserData}
+      </form>
       <div className="button-bar">
         <button onClick={onCancelClick} >Cancel</button>
         <button>Save</button>
