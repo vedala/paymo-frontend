21cdd54bfd7cab4ca429084b7c6405d595d76413
Kalyan Vedala <svedala23@gmail.com>
2024-09-24 20:17:10 -0700

    Add markup SendMoney component, in progress

--- src/components/SendMoney.tsx
+++ src/components/SendMoney.tsx
@@ -1,8 +1,65 @@
+import { useState } from "react";
+
 const SendMoney = () => {
+
+  const handleFormSubmit = (e: any) => {
+    e.preventDefault();
+  }
+
+  let banks = [
+    {
+      id: 1,
+      name: "Chase",
+    },
+    {
+      id: 2,
+      name: "Regions Bank",
+    },
+  ];
+
+  let recipients = [
+    {
+      id: 1,
+      name: "Recipient-1",
+    },
+    {
+      id: 2,
+      name: "Recipient-2",
+    },
+  ];
+
+  const [bank, setBank] = useState("Select a bank");
+  const [recipient, setRecipient] = useState("Select a recipient");
+
+  const handleBankChange = (e: any) => {
+    setBank(e.target.value);
+  }
+
+  const handleRecipientChange = (e: any) => {
+    setRecipient(e.target.value);
+  }
 
   return (
     <div>
       <h2>Send Money</h2>
+      <form className="send-money" onSubmit={handleFormSubmit}>
+        <select onChange={handleBankChange}>
+          <option value="Select a bank"> -- Select a Bank -- </option>
+          {banks.map((bank) => <option value={bank.id}>{bank.name}</option>)}
+        </select>
+
+        <br />
+        <select onChange={handleRecipientChange}>
+          <option value="Select a recipient">-- Select a Recipient --</option>
+          {recipients.map((recipient) => <option value={recipient.id}>{recipient.name}</option>)}
+        </select>
+
+        <br />
+        <input pattern="\d+.\d\d" required placeholder="Enter Amount"></input>
+
+        <br />
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
