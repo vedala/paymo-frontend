3de48edf6a6635cb68236d9f4195e1a7de515832
Kalyan Vedala <svedala23@gmail.com>
2024-10-07 21:20:15 -0700

    Create MoovTerms component

---
+++ src/components/MoovTerms.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function MoovTerms({ moovAccessToken }) {
+
+  return(
+    <moov-terms-of-service token={moovAccessToken}></moov-terms-of-service>
+  );
+}
+
+export default MoovTerms;
