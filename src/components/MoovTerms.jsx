c0d505f8b02df6a758d988d60e0a6a8a34f2d7e2
Kalyan Vedala <svedala23@gmail.com>
2024-10-10 20:48:46 -0700

    MoovTerms component changes

--- src/components/MoovTerms.jsx
+++ src/components/MoovTerms.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import { useLocation, React } from 'react';
 
-function MoovTerms({ moovAccessToken }) {
+// function MoovTerms({ moovAccessToken }) {
+function MoovTerms() {
+
+  const loc = useLocation();
+
+  const handleClick = () => {
+    console.log("Moov accept clicked, loc=", loc);
+  }
 
   return(
-    <moov-terms-of-service token={moovAccessToken} textColor="#000000" backgroundColor="#ffffff" fontSize="16px"></moov-terms-of-service>
+    // <moov-terms-of-service
+    //   token={moovAccessToken}
+    //   style={{textColor:"#000000", backgroundColor:"#ffffff", fontSize:"16px"}}
+    // >
+    // </moov-terms-of-service>
+    <button onClick={handleClick}>
+      Do you accept Moov terms of service
+    </button>
   );
 }
 
