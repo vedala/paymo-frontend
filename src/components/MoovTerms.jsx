17d3fb77bb92dd45979a70415d5284581ac64395
Kalyan Vedala <svedala23@gmail.com>
2024-10-11 21:10:20 -0700

    Generate tos token

--- src/components/MoovTerms.jsx
+++ src/components/MoovTerms.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+// import { loadMoov } from "@moovio/moov-js";
+import axios from 'axios';
 
 // function MoovTerms({ moovAccessToken }) {
 function MoovTerms() {
 
+  const PAYMO_API_URL = process.env.REACT_APP_PAYMO_API_URL;
   const loc = useLocation();
+  const moovAccessToken = loc.state.moovAccessToken;
 
-  const handleClick = () => {
+  // const moovTOSRef = useRef();
+    // await loadMoov(moovAccessToken);
+
+  // moovTOSRef.textColor = "#000000";
+  // moovTOSRef.fontSize = "18px";
+  const handleClick = async () => {
     console.log("Moov accept clicked, loc=", loc);
+
+//     const { tosToken } = await axios.get("https://api.moov.io/tos-token", {
+//       headers: {
+//         Authorization: `Bearer ${moovAccessToken}`
+//       }
+//     })
+// console.log("tosToken=", tosToken);
+    const generateTosTokenResponse = await axios.post(`${PAYMO_API_URL}/api/generate-tos-token`, {
+        moovAccessToken
+      },
+      {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      });
+
+console.log("generateTosTokenResponse=", generateTosTokenResponse);
+    const tosToken = generateTosTokenResponse.data.tosToken;
+console.log("tosToken=", tosToken);
   }
 
   return(
